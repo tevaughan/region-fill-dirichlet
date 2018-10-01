200e215f24f98d7b5de8277191b8367328ea2d38
Vaughan, Thomas E <tevaugha@ball.com>
2018-10-01 16:45:31 -0600

    cleanup

--- image.hpp
+++ image.hpp
@@ -1,24 +1,13 @@
 
+#ifndef REGFILL_IMAGE_HPP
+#define REGFILL_IMAGE_HPP
+
+#include "pcoord.hpp"
 #include "pgm-header.hpp"
 #include <fstream>
 #include <vector>
 
-#ifndef REGFILL_IMAGE_HPP
-#define REGFILL_IMAGE_HPP
-
 namespace regfill {
-
-struct pcoord {
-  uint16_t col;
-  uint16_t row;
-
-  unsigned lin(unsigned num_cols) const { return row * num_cols + col; }
-
-  void lin(unsigned num_cols, unsigned new_lin) {
-    col = new_lin % num_cols;
-    row = new_lin / num_cols;
-  }
-};
 
 class image {
   std::vector<float> pix_;
@@ -26,68 +15,69 @@ class image {
 
 public:
   image() = default;
-
-  image(uint16_t nc, uint16_t nr, float v = 0.0f)
-      : pix_(nc * nr, v), num_cols_(nc) {}
-
-  std::istream &read(std::istream &is) {
-    pgm_header h(is);
-    num_cols_ = h.num_cols();
-    unsigned const num_pix = h.num_cols() * h.num_rows();
-    pix_.resize(num_pix);
-    bool found_max = false;
-    for (unsigned i = 0; i < num_pix; ++i) {
-      pix_[i] = is.get();
-      if (!is) {
-        throw format("error reading byte %u of image (after header)", i);
-      }
-      if (pix_[i] > h.max_val()) {
-        throw format("max val %f (at %d) > %d", pix_[i], i, h.max_val());
-      } else if (pix_[i] == h.max_val()) {
-        found_max = true;
-      }
-    }
-    if (found_max == false) {
-      throw format("max val %d > value of every pixel", h.max_val());
-    }
-    return is;
-  }
-
-  image(std::string fn) {
-    std::ifstream ifs(fn);
-    if (!ifs) {
-      throw "problem opening '" + fn + "'";
-    }
-    read(ifs);
-  }
-
+  image(std::string fn);
+  image(uint16_t nc, uint16_t nr, float v = 0.0f);
+  std::istream &read(std::istream &is);
   uint16_t num_cols() const { return num_cols_; }
   uint16_t num_rows() const { return pix_.size() / num_cols_; }
   unsigned num_pix() const { return pix_.size(); }
-
-  std::ostream &write(std::ostream &os) {
-    float max_val = 0.0f, min_val = 65536.0f;
-    for (auto i = pix_.begin(); i != pix_.end(); ++i) {
-      if (*i > max_val) {
-        max_val = *i;
-      }
-      if (*i < min_val) {
-        min_val = *i;
-      }
-    }
-    enum { MAX = 255 };
-    pgm_header(num_cols_, num_rows(), MAX).write(os);
-    float const c = (MAX + 0.99f) / (max_val - min_val);
-    for (auto i = pix_.begin(); i != pix_.end(); ++i) {
-      os.put(uint8_t(c * (*i - min_val)));
-    }
-    return os;
-  }
-
+  std::ostream &write(std::ostream &os);
   float &operator()(pcoord p) { return pix_[p.lin(num_cols_)]; }
-
   float const &operator()(pcoord p) const { return pix_[p.lin(num_cols_)]; }
 };
+
+inline image::image(std::string fn) {
+  std::ifstream ifs(fn);
+  if (!ifs) {
+    throw "problem opening '" + fn + "'";
+  }
+  read(ifs);
+}
+
+inline image::image(uint16_t nc, uint16_t nr, float v)
+    : pix_(nc * nr, v), num_cols_(nc) {}
+
+inline std::istream &image::read(std::istream &is) {
+  pgm_header h(is);
+  num_cols_ = h.num_cols();
+  unsigned const num_pix = h.num_cols() * h.num_rows();
+  pix_.resize(num_pix);
+  bool found_max = false;
+  for (unsigned i = 0; i < num_pix; ++i) {
+    pix_[i] = is.get();
+    if (!is) {
+      throw format("error reading byte %u of image (after header)", i);
+    }
+    if (pix_[i] > h.max_val()) {
+      throw format("max val %f (at %d) > %d", pix_[i], i, h.max_val());
+    } else if (pix_[i] == h.max_val()) {
+      found_max = true;
+    }
+  }
+  if (found_max == false) {
+    throw format("max val %d > value of every pixel", h.max_val());
+  }
+  return is;
+}
+
+inline std::ostream &image::write(std::ostream &os) {
+  float max_val = 0.0f, min_val = 65536.0f;
+  for (auto i = pix_.begin(); i != pix_.end(); ++i) {
+    if (*i > max_val) {
+      max_val = *i;
+    }
+    if (*i < min_val) {
+      min_val = *i;
+    }
+  }
+  enum { MAX = 255 };
+  pgm_header(num_cols_, num_rows(), MAX).write(os);
+  float const c = (MAX + 0.99f) / (max_val - min_val);
+  for (auto i = pix_.begin(); i != pix_.end(); ++i) {
+    os.put(uint8_t(c * (*i - min_val)));
+  }
+  return os;
+}
 
 } // namespace regfill
 
--- pgm-header.hpp
+++ pgm-header.hpp
@@ -15,46 +15,47 @@ class pgm_header {
 
 public:
   pgm_header() = default;
-
-  pgm_header(uint16_t c, uint16_t r, uint16_t m)
-      : num_cols_(c), num_rows_(r), max_val_(m) {}
-
-  std::istream &read(std::istream &is) {
-    std::string m;
-    is >> m;
-    if (m != "P5") {
-      throw "magic '" + m + "' not 'P5'";
-    }
-    if (!(is >> num_cols_)) {
-      throw std::string("problem reading num_cols");
-    }
-    if (!(is >> num_rows_)) {
-      throw std::string("problem reading num_rows");
-    }
-    if (!(is >> max_val_)) {
-      throw std::string("problem reading max_val");
-    }
-    char c;
-    if (!is.get(c)) {
-      throw std::string("problem reading character after maxval");
-    }
-    if (c != ' ' && c != '\t' & c != '\n') {
-      throw format("character %X after maxval not white space", c);
-    }
-    return is;
-  }
-
+  pgm_header(uint16_t c, uint16_t r, uint16_t m);
   pgm_header(std::istream &is) { read(is); }
-
-  std::ostream &write(std::ostream &os) {
-    return os << format("P5\n%u %u\n%u\n", num_cols_, num_rows_, max_val_);
-  }
-
+  std::istream &read(std::istream &is);
+  std::ostream &write(std::ostream &os);
   uint16_t num_cols() const { return num_cols_; }
   uint16_t num_rows() const { return num_rows_; }
   uint16_t max_val() const { return max_val_; }
   unsigned num_pix() const { return num_cols_ * num_rows_; }
 };
+
+inline pgm_header::pgm_header(uint16_t c, uint16_t r, uint16_t m)
+    : num_cols_(c), num_rows_(r), max_val_(m) {}
+
+inline std::istream &pgm_header::read(std::istream &is) {
+  std::string m;
+  is >> m;
+  if (m != "P5") {
+    throw "magic '" + m + "' not 'P5'";
+  }
+  if (!(is >> num_cols_)) {
+    throw std::string("problem reading num_cols");
+  }
+  if (!(is >> num_rows_)) {
+    throw std::string("problem reading num_rows");
+  }
+  if (!(is >> max_val_)) {
+    throw std::string("problem reading max_val");
+  }
+  char c;
+  if (!is.get(c)) {
+    throw std::string("problem reading character after maxval");
+  }
+  if (c != ' ' && c != '\t' & c != '\n') {
+    throw format("character %X after maxval not white space", c);
+  }
+  return is;
+}
+
+inline std::ostream &pgm_header::write(std::ostream &os) {
+  return os << format("P5\n%u %u\n%u\n", num_cols_, num_rows_, max_val_);
+}
 
 } // namespace regfill
 
