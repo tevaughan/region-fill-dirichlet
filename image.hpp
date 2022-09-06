400dd23d248d6361e9c4157d697ca16f99d14b32
Vaughan, Thomas E <Thomas.Vaughan@ballaerospace.com>
2022-09-06 13:13:49 -0600

    Factored out neighbors.

--- image.hpp
+++ image.hpp
@@ -152,6 +152,7 @@ public:
 
 } // namespace regfill
 
+#include "neighbors.hpp"        // neighbors
 #include "pgm-header.hpp"       // pgm_header
 #include "threshold-coords.hpp" // threshold_coords
 #include <eigen3/Eigen/Dense>   // Triplet, VectorXd
@@ -366,34 +367,6 @@ void image::check_mask_size(image const &mask) {
     throw format("%ux%u for image, but %ux%u for mask", inc, inr, mnc, mnr);
   }
 }
-
-
-/// Offsets and flags for neighbors of a pixel.
-struct neighbors {
-  uint16_t const rb; ///< Row-offset below central pixel.
-  uint16_t const rt; ///< Row-offset above central pixel.
-  uint16_t const cl; ///< Column-offset left of central pixel.
-  uint16_t const cr; ///< Column-offset right of central pixel.
-  bool const     fb; ///< True only if there be row below central pixel.
-  bool const     ft; ///< True only if there be row above central pixel.
-  bool const     fl; ///< True only if there be column left of central pixel.
-  bool const     fr; ///< True only if there be column right of central pixel.
-
-public:
-  /// Initialize offsets and flags for neighbors.
-  /// \param nc   Number of columns in mask.
-  /// \param nr   Number of rows in mask.
-  /// \param cen  Rectangular coordinates of pixel central to neighbors.
-  neighbors(int nc, int nr, pcoord cen):
-      rb(cen.row + 1),
-      rt(cen.row - 1),
-      cl(cen.col - 1),
-      cr(cen.col + 1),
-      fb(int(cen.row) < nr - 1),
-      ft(cen.row > 0),
-      fl(cen.col > 0),
-      fr(int(cen.col) < nc - 1) {}
-};
 
 
 class cholesky_coefs {
---
+++ neighbors.hpp
@@ -0,0 +1,46 @@
+/// \file       neighbors.hpp
+/// \copyright  2022 Thomas E. Vaughan.  See terms in LICENSE.
+/// \brief      Definition of regfill::neighbors.
+
+#ifndef REGFILL_NEIGHBORS_HPP
+#define REGFILL_NEIGHBORS_HPP
+
+#include "pcoord.hpp" // pcoord
+#include <cstdint>    // uint16_t
+
+namespace regfill {
+
+
+/// Offsets and flags for neighbors of a pixel.
+struct neighbors {
+  uint16_t const rb; ///< Row-offset below central pixel.
+  uint16_t const rt; ///< Row-offset above central pixel.
+  uint16_t const cl; ///< Column-offset left of central pixel.
+  uint16_t const cr; ///< Column-offset right of central pixel.
+  bool const     fb; ///< True only if there be row below central pixel.
+  bool const     ft; ///< True only if there be row above central pixel.
+  bool const     fl; ///< True only if there be column left of central pixel.
+  bool const     fr; ///< True only if there be column right of central pixel.
+
+public:
+  /// Initialize offsets and flags for neighbors.
+  /// \param nc   Number of columns in mask.
+  /// \param nr   Number of rows in mask.
+  /// \param cen  Rectangular coordinates of pixel central to neighbors.
+  neighbors(int nc, int nr, pcoord cen):
+      rb(cen.row + 1),
+      rt(cen.row - 1),
+      cl(cen.col - 1),
+      cr(cen.col + 1),
+      fb(int(cen.row) < nr - 1),
+      ft(cen.row > 0),
+      fl(cen.col > 0),
+      fr(int(cen.col) < nc - 1) {}
+};
+
+
+} // namespace regfill
+
+#endif // ndef REGFILL_NEIGHBOR_HPP
+
+// EOF
