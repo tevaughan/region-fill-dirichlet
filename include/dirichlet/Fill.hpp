0b3e992931c1d225f894184e60ec96a23df34e8f
Vaughan, Thomas E <Thomas.Vaughan@ballaerospace.com>
2022-09-10 21:30:11 -0600

    Cleanup.

--- include/dirichlet/Fill.hpp
+++ include/dirichlet/Fill.hpp
@@ -5,7 +5,7 @@
 #ifndef DIRICHLET_FILL_HPP
 #define DIRICHLET_FILL_HPP
 
-#include <eigen3/Eigen/Dense> // ArrayX2i, ArrayXf, ArrayXXi, ArrayXi, Map
+#include "impl/initCoords.hpp" // initCoords(), Coords, Eigen/Dense
 
 /// Namespace for code that solves Dirichlet-problem for zero-valued Laplacian
 /// across specified pixels in image.
@@ -14,21 +14,6 @@ namespace dirichlet {
 
 using Eigen::ArrayXf;  // For solution.
 using Eigen::ArrayXXi; // For coordinates-map.
-
-
-/// Type used by Fill for receiving row-column pairs, each holding rectangular
-/// coordinates of pixel to be filled in (as hole might be filled in) according
-/// to Dirichlet-problem.  If `coords` be instance of type `%Coords`, then
-///
-/// - `coords(i,0)` holds offset of row    of `i`th pixel to be filled, and
-/// - `coords(i,1)` holds offset of column of `i`th pixel to be filled.
-///
-/// Every pixel specified by `coords` must be in interior of image; no pixel
-/// specified in `coords` may be at edge of image.  If region that must be
-/// filled in extend to edge of image, then pixels along edge must first be
-/// filled in by other means, and, after that, Fill may be instantiated to fill
-/// in interior holes.
-using Coords= Eigen::ArrayX2i;
 
 
 /// Fill one or more holes of any size in image by solving Dirichlet-problem
@@ -42,33 +27,6 @@ class Fill {
   /// Map from rectangular coordinates of pixel in solution to offset in array
   /// returned by x().  See documentation for coordsMap().
   ArrayXXi coordsMap_;
-
-  /// Verify that coordinates support well defined solution.
-  ///
-  /// Return false if any coordinates be out of bounds.  Every pixel on edge of
-  /// image is out of bounds.  See documentation for Coords.
-  ///
-  /// \param coords  Coordinates of each pixel to be Dirichlet-filled.
-  /// \param width   Number of columns in image.
-  /// \param height  Number of rows    in image.
-  /// \return        True only if coordinates support well defined solution.
-  ///
-  static bool
-  coordsGood(Coords const &coords, unsigned width, unsigned height);
-
-  /// Compute initializer for value returned by coordsMap().
-  ///
-  /// Empty matrix is returned if any coordinates be out of bounds or in corner
-  /// of image.  Every pixel on edge of image is out of bounds.  See
-  /// documentation for Coords.
-  ///
-  /// \param coords  Coordinates of each pixel to be Dirichlet-filled.
-  /// \param width   Number of columns in image.
-  /// \param height  Number of rows    in image.
-  /// \return        Initializer for array returned by coordsMap().
-  ///
-  static ArrayXXi
-  initCoords(Coords const &coords, unsigned width, unsigned height);
 
 public:
   /// Analyze image, and, for each pixel whose location is specified in
@@ -172,35 +130,6 @@ using std::cerr;
 using std::endl;
 
 
-inline bool
-Fill::coordsGood(Coords const &coords, unsigned width, unsigned height) {
-  auto roob= (coords.col(0) <= 0) || (coords.col(0) >= height - 1);
-  auto coob= (coords.col(1) <= 0) || (coords.col(1) >= width - 1);
-  if((roob || coob).cast<int>().sum() > 0) {
-    char const w[]= "Fill::coordsGood: WARNING: ";
-    for(int i= 0; i < roob.size(); ++i) {
-      int const r= coords(i, 0);
-      int const c= coords(i, 1);
-      if(roob[i]) cerr << w << "r(" << i << ")=" << r << " OOB" << endl;
-      if(coob[i]) cerr << w << "c(" << i << ")=" << c << " OOB" << endl;
-    }
-    return false;
-  }
-  return true;
-}
-
-
-inline ArrayXXi
-Fill::initCoords(Coords const &coords, unsigned width, unsigned height) {
-  if(!coordsGood(coords, width, height)) return ArrayXXi();
-  ArrayXXi      cmap(ArrayXXi::Constant(height, width, -1));
-  ArrayXi const lin= coords.col(0) + coords.col(1) * height;
-  Map<ArrayXi>  m(&cmap(0, 0), height * width, 1);
-  m(lin)= ArrayXi::LinSpaced(coords.rows(), 0, coords.rows() - 1);
-  return cmap;
-}
-
-
 template<typename C>
 Fill::Fill(
       Coords const &coords,
@@ -208,7 +137,7 @@ Fill::Fill(
       unsigned      width,
       unsigned      height,
       unsigned      numComps):
-    coordsMap_(initCoords(coords, width, height)) {
+    coordsMap_(impl::initCoords(coords, width, height)) {
   if(coordsMap_.rows() == 0) return;
   // TBS
 }
---
+++ include/dirichlet/impl/initCoords.hpp
@@ -0,0 +1,60 @@
+/// \file       include/dirichlet/impl/initCoords.hpp
+/// \copyright  2022 Thomas E. Vaughan.  See terms in LICENSE.
+/// \brief      Definition of dirichlet::impl::initCoords().
+
+#ifndef DIRICHLET_IMPL_INIT_COORDS_HPP
+#define DIRICHLET_IMPL_INIT_COORDS_HPP
+
+#include "../Coords.hpp" // Coords and classes in Eigen/Dense
+#include <iostream>      // cerr, endl
+
+/// Namespace for code that solves Dirichlet-problem for zero-valued Laplacian
+/// across specified pixels in image.
+namespace dirichlet::impl {
+
+
+using Eigen::ArrayXi;
+using Eigen::ArrayXXi;
+using Eigen::Map;
+using std::cerr;
+using std::endl;
+
+
+/// Compute initializer for value returned by Fill::coordsMap().
+///
+/// Empty matrix is returned if any coordinates be out of bounds.  Every pixel
+/// on edge of image is out of bounds.  See documentation for Coords.
+///
+/// \param coords  Coordinates of each pixel to be Dirichlet-filled.
+/// \param width   Number of columns in image.
+/// \param height  Number of rows    in image.
+/// \return        Initializer for array returned by Fill::coordsMap().
+///
+inline ArrayXXi
+initCoords(Coords const &coords, unsigned width, unsigned height) {
+  auto roob= (coords.col(0) <= 0) || (coords.col(0) >= height - 1);
+  auto coob= (coords.col(1) <= 0) || (coords.col(1) >= width - 1);
+  if((roob || coob).cast<int>().sum() > 0) {
+    char const w[]= "Fill::coordsGood: WARNING: ";
+    for(int i= 0; i < roob.size(); ++i) {
+      int const r= coords(i, 0);
+      int const c= coords(i, 1);
+      if(roob[i]) cerr << w << "r(" << i << ")=" << r << " OOB" << endl;
+      if(coob[i]) cerr << w << "c(" << i << ")=" << c << " OOB" << endl;
+    }
+    return ArrayXXi();
+  }
+  ArrayXXi      cmap(ArrayXXi::Constant(height, width, -1));
+  ArrayXi const lin= coords.col(0) + coords.col(1) * height;
+  Map<ArrayXi>  m(&cmap(0, 0), height * width, 1);
+  m(lin)= ArrayXi::LinSpaced(coords.rows(), 0, coords.rows() - 1);
+  return cmap;
+}
+
+
+} // namespace dirichlet
+
+#endif // ndef DIRICHLET_IMPL_INIT_COORDS_HPP
+
+// EOF
+
